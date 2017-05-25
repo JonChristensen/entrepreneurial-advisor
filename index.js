1936f1bb1bff3d5a3af37857873eccffd52eb5cf
Jon Christensen <jon@dv-mobile.com>
2017-05-25 00:08:21 -0600

    updates

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ var handlers = {
             console.log("Just trying to log something.  Anything.")
             //console.log('error:', error); // Print the error if one occurred
             //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            self.emit(':tell', "hi team! good work.  are you tired too?"); // Print the HTML for the Google homepage.
+            console.log(body)
+            self.emit(':tell', body); // Print the HTML for the Google homepage.
             
         });
         // inform user the new account number
